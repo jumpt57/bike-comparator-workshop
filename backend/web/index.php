1ca635608c3e53eb0ff2c2bdc421470e7d854e37
Xavier <laffargue.xavier@gmail.com>
2016-11-14 15:11:55 +0100

    Backend test

--- backend/web/index.php
+++ backend/web/index.php
@@ -5,7 +5,30 @@ $app = new Silex\Application();
 
 // ... definitions
 
-$app['debug'] = true;
-$app->run();
+//$app['debug'] = true;
 
+try {
+$pdo = new PDO("mysql:host=anarkhief.fr; dbname=anar33_comparateur", "anar33_comparat", "comparateur123");
+} catch (PDOException $ex) { echo $ex->getMessage(); die(); }
+
+$app->get('/hello/{name}', function($name) use ($app) {
+	echo '<h1>Hello ' . $name . '</h1>';
+});
+
+$app->get('/manufactures?filtre={name}', function ($name) use ($app) {
+	$arrayManufactures = array();
+	$arrayManufactures['success'] = 1;
+	$arrayManufactures['manufactures'] = array();
+
+  	$query = "SELECT * FROM marques WHERE nom like '%".$name."%'";
+  	$result = $pdo->query($query);
+  	while( $resultat = $resultats->fetch()) {
+  		array_push($arrayManufactures['manufactures'], $resultat->name);
+
+  	}
+	return print(json_encode($arrayManufactures));
+});
+
+
+$app->run();
 ?>
